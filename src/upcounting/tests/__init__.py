7de9b615c618030c69486e7d5d42c39e3630c105
Johannes <johannes-programming@mailfence.com>
2025-07-16 19:47:09 +0200

    Version 1.0.2+local.3

--- src/upcounting/tests/__init__.py
+++ src/upcounting/tests/__init__.py
@@ -3,8 +3,7 @@ import unittest
 __all__ = ["test"]
 
 
-def test() -> unittest.TextTestRunner:
-    "This function runs all the tests."
+def test():
     loader = unittest.TestLoader()
     tests = loader.discover(start_dir="upcounting.tests")
     runner = unittest.TextTestRunner()
