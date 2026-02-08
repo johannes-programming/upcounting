483b5216526d227067a114bf9bbef404d4b269af
Johannes <johannes.programming@gmail.com>
2026-02-08 17:54:29 +0100

    Version 1.0.6

--- src/upcounting/tests/__init__.py
+++ src/upcounting/tests/__init__.py
@@ -4,8 +4,8 @@ __all__ = ["test"]
 
 
 def test() -> unittest.TextTestResult:
-    loader: unittest.TestLoader = unittest.TestLoader()
-    tests: unittest.TestSuite = loader.discover(start_dir="upcounting.tests")
-    runner: unittest.TextTestRunner = unittest.TextTestRunner()
-    result: unittest.TextTestResult = runner.run(tests)
-    return result
+    loader: unittest.TestLoader
+    tests: unittest.TestSuite
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir="upcounting.tests")
+    return unittest.TextTestRunner().run(tests)
